742dd944dfcea4eae011fb06586b935a02fdf04b
aightech <long.devillard@gmail.com>
2020-01-21 11:26:59 +0100

    updated

--- include/Lexium32A_canopen.h
+++ include/Lexium32A_canopen.h
@@ -2,64 +2,31 @@
 #define _LEXIUM32A_CANOPEN_H_
 
 #include "canopen_socket.h"
-#include "LXM32A_CANopen_register.h"
+//#include "LXM32A_CANopen_register.h"
 #include <string>
 
-#define NB_PARAM 5
 
-#define IND_CANaddress          0x30410002
-#define IND_CANbaud             0x30410003
-#define IND__CanDiag            0x30410006
-#define IND__ManuSdoAbort       0x3041000A
-#define IND_CANpdo1Event        0x3041000B
-#define IND_CANpdo2Event        0x3041000C
-#define IND_CANpdo3Event        0x3041000D
-#define IND_CANpdo4Event        0x3041000E
+#define REG_CANaddress          0x30410002
+#define REG_CANbaud             0x30410003
 
-#define IND__CTRL_KPid          0x30110001
-#define IND__CTRL_TNid          0x30110002
-#define IND__CTRL_KPiq          0x30110003
-#define IND__CTRL_TNiq          0x30110004
-#define IND_CTRL_vPIDDTime      0x30110005
-#define IND_CTRL_vPIDDPart      0x30110006
-#define IND_CTRL_TAUnact        0x30110008
-#define IND_CTRL_SpdFric        0x30110009
-#define IND_CTRL_KFAcc          0x3011000A
-#define IND_CTRL_I_max          0x3011000C
-#define IND_LIM_I_maxQSTP       0x3011000D
-#define IND_LIM_I_maxHalt       0x3011000E
-#define IND_CTRL_I_max_fw       0x3011000F
-#define IND_CTRL_v_max          0x30110010
-#define IND_CTRL_ParChgTime     0x30110014
-#define IND_CTRL_GlobGain       0x30110015
-#define IND_CTRL_ParSetCopy     0x30110016
-#define IND__CTRL_ActParSet     0x30110017
-#define IND_CTRL_PwrUpParSet    0x30110018
-#define IND_CTRL_SelParSet      0x30110019
-#define IND_CLSET_ParSwiCond    0x3011001A
-#define IND_CLSET_winTime       0x3011001B
-#define IND_CLSET_p_DiffWin     0x3011001C
-#define IND_CLSET_v_Threshol    0x3011001D
-#define IND_CTRL_VelObsActiv    0x30110022
-#define IND_CTRL_VelObsDyn      0x30110023
-#define IND_CTRL_VelObsInert    0x30110024
-#define IND_CLSET_p_DiffWin_usr 0x30110025
-#define IND_CTRL_SmoothCurr     0x30110026
+#define REG__DCOMstatus         0x60410000
+#define REG_DCOMopmode          0x60600000
+#define REG_DCOMcontrol         0x60400000
 
-#define IND__IO_act             0x30080001
-#define IND__IO_DI_act          0x3008000F
-#define IND_BRK_release         0x3008000A
-#define IND__IO_DQ_act          0x30080010
-#define IND_IO_DQ_set           0x30080011
-#define IND_DI_0_Debounce       0x30080020
-#define IND_DI_1_Debounce       0x30080021
-#define IND_DI_2_Debounce       0x30080022
-#define IND_DI_3_Debounce       0x30080023
-#define IND__IO_STO_act         0x30080026
-#define IND_IOsigVelLim         0x30080027
-#define IND_IOsigCurrLim        0x30080028
+#define REG_PPp_target          0x607A0000
+#define REG_PPv_target          0x60810000
+#define REG_PPoption            0x60F20000
 
-#define PARAM_NAME {"hey", "hoy"}
+
+#define MODE_ProfilePosition 1
+
+#define OP_SHUTDOWN     0x0006
+#define OP_SWITCHON     0x0007
+#define OP_DISABLEVOL   0x0000
+#define OP_QUICKSTEP    0x0002
+#define OP_DISABLEOP    0x0007
+#define OP_ENABLEOP     0x000F
+#define OP_FAULTRESEST  0x0080
 
 
 
@@ -68,9 +35,10 @@
 class LXM32 
 {
  public:
- LXM32(const char* ifname,uint16_t can_address, bool verbose = false): m_can(ifname,verbose), m_can_address(can_address), m_verbose(verbose)
+ LXM32(const char* ifname,uint16_t can_id, bool verbose = false): m_can(ifname,verbose), m_can_id(can_id), m_verbose(verbose)
     {
-      
+      get_param();
+      print_param();
     };
 
   
@@ -85,8 +53,26 @@ class LXM32
 
     uint16_t index = 0x1000;
     uint8_t  subindex = 0;
-    get_param();
-};
+    //get_param();
+    print_param();
+  };
+
+  void new_pos(int32_t pos, bool abs, uint32_t spd=0)
+  {
+    m_PPp_target = pos;
+    m_PPv_target = (spd!=0)?spd:m_PPv_target;
+    m_PPoption = (abs)?2:0;
+
+    m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_W, REG_PPp_target, m_PPp_target);
+    m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_W, REG_PPv_target, m_PPv_target);
+    m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_W, REG_PPoption, m_PPoption);
+
+    m_dcom_mode = MODE_ProfilePosition;
+    m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_W, REG_DCOMopmode, m_dcom_mode);
+    
+    m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_W, REG_DCOMcontrol, OP_ENABLEOP);
+    
+  };
 
 
   void stop()
@@ -94,19 +80,77 @@ class LXM32
 
   void get_param()
   {
-    m_can.send_SDO(m_can_address , FRAME_SIZE, SDO_R, IND_CANaddress);
-    m_can.send_SDO(m_can_address , FRAME_SIZE, SDO_R, IND_CANbaud);
+    m_can_id   = (uint16_t)m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_R, REG_CANaddress);
+    m_can_baud = (uint16_t)m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_R, REG_CANbaud);
+    m_dcom_status = (uint16_t)m_can.send_SDO(m_can_id , FRAME_SIZE, SDO_R, REG__DCOMstatus);
+  };
+
+  void print_param()
+  {
     
-  }
+    printf("> LXM32A Parameters\n");
+    printf("\t>> CANopen bus info:\n");
+    printf("\t\t >>> Node_id: %03d\n", m_can_id);
+    printf("\t\t >>> Baud: %d kps\n",  m_can_baud);
+    printf("\t>> Operating State info:\n");
+    
+    
+    if(m_dcom_status & 0x0008)//FAULT
+      {
+	m_op_state=6;
+	if(m_dcom_status & 0x0007)
+	  m_op_state=7;
+      }
+    else if(m_dcom_status & 0x0040)//DISABLE
+      m_op_state=0;
+    else if((m_dcom_status&0x0020)==0)
+      m_op_state=5;
+    else
+      {
+	m_op_state= (m_dcom_status&0x0007);
+	m_op_state= (m_op_state==0x00)?0:(m_op_state==0x01)?1:(m_op_state==0x03)?2:3;
+	m_op_state+=1;
+      }
+    printf("\t\t>>> [ %s ]\n",m_op_state_str[m_op_state]);
+
+    printf("\t\t>>> Voltage: %s\n",(m_dcom_status&0x10)?"ON":"OFF");
+    printf("\t\t>>> Error Msg: %s\n",(m_dcom_status&0x80)?"YES":"NO");
+
+    printf("\t\t>>> Halt: %s\n",(m_dcom_status&0x0100)?"ACTIVATED":"DESACTIVATED");
+
+    printf("\t\t>>> Field bus: %s\n",(m_dcom_status&0x0200)?"ACTIVATED":"DESACTIVATED");
+    printf("\t\t>>> Error : %s\n",(m_dcom_status&0x2000)?"YES":"NO");
+
+    printf("\t\t>>> Operating : %s\n",(m_dcom_status&0x4000)?"YES":"NO");
+    printf("\t\t>>> Valid Zero Point : %s\n",(m_dcom_status&0x4000)?"YES":"NO");
+    
+    
+  };
 
   
  private:
   Canopen_socket m_can;
   bool m_verbose;
-  uint16_t m_can_address;
-  uint32_t    m_param_val[NB_PARAM];
-  std::string m_param_nam[2]=PARAM_NAME;
-  uint32_t    m_param_reg[NB_PARAM];
+  uint16_t m_can_id;
+  uint16_t m_can_baud;
+  uint16_t m_dcom_status;
+
+  int8_t m_dcom_mode; 
+
+  //Position profil param
+  int32_t  m_PPp_target;
+  uint32_t m_PPv_target=60;
+  uint16_t m_PPoption;
+
+  uint8_t m_op_state;
+  const char* m_op_state_str[8]={"DISABLED",
+				 "NOT READY",
+				 "READY",
+				 "ON",
+				 "ENABLED",
+				 "QUICK STOP",
+				 "FAULT",
+				 "FAULT, REACTION ACTIVE"};
   
   
 };
